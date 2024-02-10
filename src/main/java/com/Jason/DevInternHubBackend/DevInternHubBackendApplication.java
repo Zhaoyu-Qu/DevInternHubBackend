3dda3182ca3086456cda00ace10bad82a6dd1a0c
Zhaoyu Qu <113986077+Zhaoyu-Qu@users.noreply.github.com>
2024-02-10 19:46:16 +0800

    initial commit

---
+++ src/main/java/com/Jason/DevInternHubBackend/DevInternHubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Jason.DevInternHubBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevInternHubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevInternHubBackendApplication.class, args);
+	}
+
+}
